c9c51de40eff8d867c238de1e836fb1885f522a3
Volodymyr Hudzula <110738058+NeosFCDD@users.noreply.github.com>
2023-01-16 19:16:02 +0200

    Done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,27 +1,27 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items.js';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from "./gallery-items.js";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-const galleryList = document.querySelector('.gallery');
+const galleryList = document.querySelector(".gallery");
 const imagesMarkup = createImagesMarkup(galleryItems);
 
-galleryList.insertAdjacentHTML('beforeend', imagesMarkup);
+galleryList.insertAdjacentHTML("beforeend", imagesMarkup);
 
 function createImagesMarkup(galleryItems){
     return galleryItems.map(({preview, original, description})=>{
-        return `<li><a class='gallery__link' href='${original}'>
-        <img  class='gallery__image' src ='${preview}' alt ='${description}'>
-        </a></li>`;}).join('');
+        return `<li><a class="gallery__link" href="${original}">
+        <img  class="gallery__image" src ="${preview}" alt ="${description}">
+        </a></li>`;}).join("");
 }
 
-const gallery = new SimpleLightbox('.gallery a', {
+const gallery = new SimpleLightbox(".gallery a", {
     captions: true,
-    captionsData: 'alt',
+    captionsData: "alt",
     captionDelay: 250,
 });
 
-gallery.on('show.simplelightbox', function () {});
+gallery.on("show.simplelightbox", function () {});
 
 console.log(galleryItems);
